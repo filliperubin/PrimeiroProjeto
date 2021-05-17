01a9967415e092c9386411ef73a6a9f4fa210f44
filliperubin <Fillipe@192.168.15.7>
2021-05-17 14:44:54 -0300

    Adicionado Mensagem Olá Mundo na classe principal

--- src/main/java/com/mycompany/primeiroprojetogithub/Principal.java
+++ src/main/java/com/mycompany/primeiroprojetogithub/Principal.java
@@ -12,6 +12,6 @@ package com.mycompany.primeiroprojetogithub;
 public class Principal {
     // Método Principal
     public static void main(String[] args) {
-        
+         System.out.println("Olá Mundo!");    
     }
 }
